f06579b3c3b06b7bec99a463fbec12fad7da773f
Jonathan <jonathanmoorevla@gmail.com>
2023-05-28 15:59:04 -0500

    Add newconvo method/route

--- client/src/components/NewMessageForm.js
+++ client/src/components/NewMessageForm.js
@@ -1,22 +1,23 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
+import { UserContext } from "../contexts/UserContext"
 
-function NewMessageForm({handleSendMessage, recipient, convo}) {
+function NewMessageForm({handleSendMessage, recipient, convo = null}) {
 
     const [message, setMessage] = useState("")
-    
-    function handleMessagePost(e) {
-        e.preventDefault()
-        let messageObj = {
-            conversation_id: convo,
-            recipient_id: recipient,
-            text: message
-        }
-        fetch("/new_message", {
+    const {user} = useContext(UserContext)
+
+    let messageBase = {
+        recipient_id: recipient,
+        text: message
+    }
+
+    function newConvoMessage() {
+        fetch("/new_convo", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(messageObj)
+            body: JSON.stringify(messageBase)
         })
         .then((r) => {
             if (r.ok) {
@@ -30,9 +31,44 @@ function NewMessageForm({handleSendMessage, recipient, convo}) {
         })
     }
 
+    function newMessage() {
+        fetch("/new_message", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({...messageBase, conversation_id: convo})
+        })
+        .then((r) => {
+            if (r.ok) {
+                r.json().then((data) => {
+                    handleSendMessage(data)
+                    setMessage("")
+                })
+            } else {
+                r.json().then((err) => console.log(err))
+            }
+        })
+    }
+
+    function handleSendMessage(e) {
+        e.preventDefault()
+        if (user.convos.some((c) => c.two_users.find((u) => u.id !== user.id).id === recipient)) {
+            newMessage()
+            // console.log("just new message")
+        } else {
+            newConvoMessage()
+            // console.log("new convo")
+        }
+    }
+
+
+
+
+
     return(
         <div>
-            <form onSubmit={handleMessagePost}>
+            <form onSubmit={handleSendMessage}>
                 <input onChange={(e) => setMessage(e.target.value)} value={message} />
                 <button type="submit">Send</button>
             </form>
