19e990bd71ace0a267c4140fd4f451740ee1aed6
Jonathan <jonathanmoorevla@gmail.com>
2023-06-26 23:29:29 -0500

    Update seeds, fix books checked_out and exchanges

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,7 @@ require "faker"
 puts "🌱 Seeding public_library tables..."
 
 neighborhoods = ["Uptown", "Edgewater", "Ravenswood", "The Loop", "Hyde Park", "Rogers Park", "Lakeview", "Kenwood", "Bronzeville"]
-genres = ["Science Fiction", "Mystery", "Romance", "Thriller", "Horror", "Fantasy",
-    "Historical Fiction", "Young Adult", "Biography", "Self-Help", "Academic"]
+genres = ["Science Fiction", "Mystery", "Romance", "Thriller", "Horror", "Fantasy", "Historical Fiction", "Young Adult", "Biography", "Self-Help", "Academic"]
 
 User.create(
     username: "TestingUser1", neighborhood: neighborhoods[rand(0..8)], password: "testing", password_confirmation: "testing",
@@ -22,6 +21,22 @@ User.create(
     username: "TestingUser4", neighborhood: neighborhoods[rand(0..8)], password: "testing", password_confirmation: "testing",
     bio: Faker::Lorem.sentence(word_count: 10), fav_author: Faker::Book.author, fav_genre: genres[rand(0..10)]
 )
+User.create(
+    username: "TestingUser5", neighborhood: neighborhoods[rand(0..8)], password: "testing", password_confirmation: "testing",
+    bio: Faker::Lorem.sentence(word_count: 10), fav_author: Faker::Book.author, fav_genre: genres[rand(0..10)]
+)
+User.create(
+    username: "TestingUser6", neighborhood: neighborhoods[rand(0..8)], password: "testing", password_confirmation: "testing",
+    bio: Faker::Lorem.sentence(word_count: 10), fav_author: Faker::Book.author, fav_genre: genres[rand(0..10)]
+)
+User.create(
+    username: "TestingUser7", neighborhood: neighborhoods[rand(0..8)], password: "testing", password_confirmation: "testing",
+    bio: Faker::Lorem.sentence(word_count: 10), fav_author: Faker::Book.author, fav_genre: genres[rand(0..10)]
+)
+User.create(
+    username: "TestingUser8", neighborhood: neighborhoods[rand(0..8)], password: "testing", password_confirmation: "testing",
+    bio: Faker::Lorem.sentence(word_count: 10), fav_author: Faker::Book.author, fav_genre: genres[rand(0..10)]
+)
 users = User.all
 
 200.times do
@@ -30,6 +45,13 @@ users = User.all
         hardback: Faker::Boolean.boolean, notes: Faker::Lorem.sentence(word_count: 10), checked_out: false, hidden: false
     )
 end
+100.times do 
+    Book.create(
+        user_id: users[rand(4..7)].id, title: Faker::Book.title, author: Faker::Book.author, genre: genres[rand(0..10)], num_pages: rand(50..350),
+        hardback: Faker::Boolean.boolean, notes: Faker::Lorem.sentence(word_count: 10), checked_out: false, hidden: false
+    )
+end
+
 # byebug
 books = Book.all
 books0 = Book.all.select {|b| b.user_id == users[0].id}
@@ -52,7 +74,7 @@ Exchange.create(user_id: users[2].id, book_id: books1[rand(20..29)].id, approved
 Exchange.create(user_id: users[2].id, book_id: books0[rand(30..39)].id, approved: false, received: false, returned: false, complete: false)
 
 exchs = Exchange.all
-exchs.each {|e| e.book.update(checked_out: true) if (e[:approved] == true && e[:complete] == false)}
+exchs.each {|e| e.book.update(checked_out: true) if (e[:complete] == false)}
 
 3.times do
     Conversation.create()
